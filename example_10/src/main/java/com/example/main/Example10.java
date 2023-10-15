79d11af107f37ed1c1d9253f02049a2e300d77c7
Deepak <104371928+Deepak2272@users.noreply.github.com>
2023-10-15 16:45:09 +0530

    Update Example10.java

--- example_10/src/main/java/com/example/main/Example10.java
+++ example_10/src/main/java/com/example/main/Example10.java
@@ -4,7 +4,7 @@ import com.example.beans.Person;
 import com.example.beans.Vehicle;
 import com.example.config.ProjectConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-//Hello deepak
+//Hello deepak!
 public class Example10 {
 
     public static void main(String[] args) {
